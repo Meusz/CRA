4113be13168841cc36fbc0b7e04bbae6dc7e861d
dxnv25 <80310999+dxnv25@users.noreply.github.com>
2021-03-18 18:36:41 +0100

    personajes

--- PL1/conocimiento.pl
+++ PL1/conocimiento.pl
@@ -1,16 +1,21 @@
-personaje(['Albert', 'Paul', 'Tom', 'Derek', 'Richard', 'Louis', 'Michael', 'Charles', 'Sam', 'Steve', 'Will', 'Anthony', 'Billy', 'Henry', 'Tiffany', 'Natalie', 'Roxanne', 'Sarah', 'Sabrina', 'Cindy', 'Emma']).
-chico(['Albert', 'Paul', 'Tom', 'Derek', 'Richard', 'Louis', 'Michael', 'Charles', 'Sam', 'Steve', 'Will', 'Anthony', 'Billy', 'Henry']).
-chica(['Tiffany', 'Natalie', 'Roxanne', 'Sarah', 'Sabrina', 'Cindy', 'Emma']).
-pelo_rubio(['Paul', 'Michael', 'Sam', 'Will', 'Anthony', 'Billy', 'Natalie', 'Roxanne', 'Emma']).
-pelo_negro(['Albert', 'Tom', 'Derek', 'Richard', 'Louis', 'Charles', 'Steve', 'Henry', 'Tiffany', 'Sarah', 'Sabrina', 'Cindy']).
-ropa_roja(['Albert', 'Paul', 'Richard', 'Louis', 'Sam', 'Steve', 'Anthony', 'Henry', 'Natalie', 'Sarah', 'Cindy']).
-ropa_verde(['Tom', 'Derek', 'Michael', 'Charles', 'Will', 'Billy', 'Tiffany', 'Roxanne', 'Sabrina', 'Emma']).
-feliz(['Albert', 'Tom', 'Richard', 'Michael', 'Charles', 'Steve', 'Will', 'Anthony', 'Tiffany', 'Natalie', 'Roxanne', 'Sabrina', 'Cindy', 'Emma']).
-triste(['Paul', 'Derek', 'Louis', 'Sam', 'Billy', 'Henry', 'Sarah']).
-ojos_azules(['Albert', 'Richard', 'Louis', 'Sam', 'Will', 'Billy', 'Natalie', 'Roxanne', 'Sabrina']).
-ojos_marrones(['Paul', 'Tom', 'Derek', 'Michael', 'Charles', 'Steve', 'Anthony', 'Henry', 'Tiffany', 'Sarah', 'Cindy', 'Emma']).
-gafas(['Albert', 'Michael', 'Charles', 'Anthony', 'Natalie', 'Sabrina']).
-no_gafas(['Paul','Tom','Derek','Richard','Louis','Sam','Steve','Will','Billy','Henry','Tiffany','Roxanne','Sarah','Cindy', 'Emma']).
+personaje(['Albert', 'Paul', 'Tom', 'Derek', 'Richard', 'Louis', 'Michael', 'Charles', 'Sam', 'Steve', 'Will', 'Anthony', 'Billy', 'Henry', 'Tiffany', 'Natalie', 'Roxanne', 'Sarah', 'Sabrina', 'Cindy', 'Emma', 'Daniel',' Rachel', 'Patrik', 'Helen', 'Robert', 'Lucy']).
+chico(['Albert', 'Paul', 'Tom', 'Derek', 'Richard', 'Louis', 'Michael', 'Charles', 'Sam', 'Steve', 'Will', 'Anthony', 'Billy', 'Henry', 'Daniel', 'Patrik', 'Robert']).
+chica(['Tiffany', 'Natalie', 'Roxanne', 'Sarah', 'Sabrina', 'Cindy', 'Emma', 'Lucy', 'Rachel', 'Helen']).
+pelo_rubio(['Paul', 'Michael', 'Sam', 'Will', 'Anthony', 'Billy', 'Natalie', 'Roxanne', 'Emma', 'Lucy', 'Daniel', 'Robert']).
+pelo_negro(['Albert', 'Tom', 'Derek', 'Richard', 'Louis', 'Charles', 'Steve', 'Henry', 'Tiffany', 'Sarah', 'Sabrina', 'Cindy', 'Rachel', 'Helen', 'Patrik']).
+ropa_roja(['Albert', 'Paul', 'Richard', 'Louis', 'Sam', 'Steve', 'Anthony', 'Henry', 'Natalie', 'Sarah', 'Cindy', 'Rachel', 'Lucy', 'Daniel']).
+ropa_verde(['Tom', 'Derek', 'Michael', 'Charles', 'Will', 'Billy', 'Tiffany', 'Roxanne', 'Sabrina', 'Emma', 'Robert', 'Helen', 'Patrik']).
+feliz(['Albert', 'Tom', 'Richard', 'Michael', 'Charles', 'Steve', 'Will', 'Anthony', 'Tiffany', 'Natalie', 'Roxanne', 'Sabrina', 'Cindy', 'Emma', 'Lucy', 'Daniel', 'Helen']).
+triste(['Paul', 'Derek', 'Louis', 'Sam', 'Billy', 'Henry', 'Sarah', 'Patrik', 'Robert', 'Rachel']).
+ojos_azules(['Albert', 'Richard', 'Louis', 'Sam', 'Will', 'Billy', 'Natalie', 'Roxanne', 'Sabrina','Robert', 'Rachel', 'Lucy', 'Daniel', 'Helen']).
+ojos_marrones(['Paul', 'Tom', 'Derek', 'Michael', 'Charles', 'Steve', 'Anthony', 'Henry', 'Tiffany', 'Sarah', 'Cindy', 'Emma', 'Patrik']).
+gafas(['Albert', 'Michael', 'Charles', 'Anthony', 'Natalie', 'Sabrina', 'Patrik', 'Daniel']).
+no_gafas(['Paul','Tom','Derek','Richard','Louis','Sam','Steve','Will','Billy','Henry','Tiffany','Roxanne','Sarah','Cindy', 'Emma','Robert', 'Rachel', 'Lucy', 'Helen']).
+gorra (['Albert' , 'Tom','Richard', 'Michael', 'Sam',  'Will', 'Billy',  'Tiffany','Roxanne', 'Cindy','Daniel',  'Patrik','Robert' ])
+no_gorra(['Paul', 'Derek', 'Louis', 'Charles','Steve', 'Anthony', 'Henry', 'Natalie',  'Sarah','Sabrina', 'Emma',' Rachel', 'Helen', 'Lucy'])
+pelo_largo(['Paul', 'Derek', 'Louis', 'Sam', 'Richard', 'Will', 'Billy','Natalie',  'Sarah','Sabrina', 'Emma', 'Tiffany','Roxanne', 'Cindy'])
+pelo_corto (['Albert' , 'Tom', 'Charles','Steve' , 'Michael', 'Anthony', 'Henry', 'Helen', 'Lucy','Daniel',  'Patrik','Robert' ,' Rachel'])
 
-preguntas(['chico','chica','pelo_rubio','pelo_negro','ropa_roja','ropa_verde','feliz','triste','ojos_azules','ojos_marrones','gafas','no_gafas']).
+
+preguntas(['chico','chica','pelo_rubio','pelo_negro','ropa_roja','ropa_verde','feliz','triste','ojos_azules','ojos_marrones','gafas','no_gafas', 'gorra', 'no_gorra', 'pelo_largo', 'pelo_corto']).
 
