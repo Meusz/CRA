99d98718be3549da7d1a077ccc1866b5036dfc05
dxnv25 <80310999+dxnv25@users.noreply.github.com>
2021-04-15 19:06:16 +0200

    3.1

--- PL2/test.pl
+++ PL2/test.pl
@@ -2,25 +2,25 @@
 %:-consult(ejercicio_3_1).
 
 
-test1:- oracion(X,[el,hombre,come,una,manzana],[]),write(X).
+test1:- oracion(X, [el,hombre,come,una,manzana],[]),write(X).
 
-test2:- oracion(X,[la,mujer,come,manzanas],[]),write(X).
+test2:- oracion(X, [la,mujer,come,manzanas],[]),write(X).
 
-test3:- oracion([maría,come,una,manzana,roja],[]).
+test3:- oracion(X, [maría,come,una,manzana,roja],[]),write(X).
 
-test4:- oracion([juan,ama,a,maría],[]).
+test4:- oracion(X, [juan,ama,a,maría],[]),write(X).
 
-test5:- oracion([el,gato,grande,come,un,ratón,gris],[]).
+test5:- oracion(X, [el,gato,grande,come,un,ratón,gris],[]),write(X).
 
-test6:- oracion([juan,estudia,en,la,universidad],[]).
+test6:- oracion(X, [juan,estudia,en,la,universidad],[]),write(X).
 
-test7:- oracion([el,alumno,ama,la,universidad],[]).
+test7:- oracion(X, [el,alumno,ama,la,universidad],[]),write(X).
 
-test8:- oracion([el,gato,come,ratones],[]).
+test8:- oracion(X, [el,gato,come,ratones],[]),write(X).
 
-test9:- oracion([la,manzana,come,un,gato],[]).
+test9:- oracion(X, [la,manzana,come,un,gato],[]),write(X).
 
-test10:- oracion([la,universidad,es,grande],[]).
+test10:- oracion(X, [la,universidad,es,grande],[]),write(X).
 
 
 
@@ -42,7 +42,7 @@ test:-test1,write('Frase 1 OK'),nl,
 
    test9,write('Frase 9 OK'),nl,
 
-   test10,write('Frase 10 OK').
+   test10,write('Frase 10 OK'), nl.
    
    
 testdibuj1:- oracion(X,[el,hombre,come,una,manzana],[]),write(X), draw(X).
