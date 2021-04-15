251683b68a929b86d479bd26c3f4aab3f1be0fff
Meusz <61017010+Meusz@users.noreply.github.com>
2021-04-15 17:34:05 +0200

    PL2 initial commit

---
+++ PL2/gramatica.pl
@@ -0,0 +1,106 @@
+% Reglas gramaticales
+%Regla de una oracion
+oracion(o(GN,GV)) --> g_nominal(GN), g_verbal(GV).
+
+o --> gn, gv.
+%Sintagma Nominal
+g_nominal(gn(N)) --> nombre(N).
+g_nominal(gn(D,N)) --> determinante(D), nombre(N).
+
+gn --> nombre.
+gn --> det, nombre.
+gn --> det, nombre, adjetivo.
+gn --> nombre, adjetivo.
+%Sintagma Preposicional
+gv --> verbo.
+gv --> verbo, gn.
+gv --> verbo, adjetivo.
+
+%Sintagma Preposicional
+g_preposicional(gprep(PREP,GN)) --> preposicion(PREP),g_nominal(GN).
+
+gprep --> prep, gn.
+
+%Sintagma Adjetival
+gadj --> _.
+%Diccionario
+
+%DETERMINANTES
+determinante(det(X)) --> [X],{det(X)}.
+
+det(el).
+det(la).
+det(lo).
+det(los).
+det(las).
+det(un).
+det(uno).
+det(una).
+det(unos).
+det(unas).
+
+
+%NOMBRES
+nombre(n(X)) --> [X],{n(X)}.
+
+n(hombre).
+n(mujer).
+n(juan).
+n(maría).
+n(manzana).
+n(manzanas).
+n(gato).
+n(ratón).
+n(alumno).
+n(universidad).
+n --> nombre_propio.
+nombre_propio(maría).
+nombre_propio(juan).
+
+
+%VERBOS
+verbo(n(X)) --> [X],{v(X)}.
+
+v(ama).
+v(come).
+v(estudia).
+
+%ADJETIVOS
+
+
+adj(roja).
+adj(negro).
+adj(grande).
+adj(gris).
+adj(pequeño).
+
+%PREPOSICIONES
+preposicion(prep(X)) --> [X],{prep(X)}.
+
+prep(a).
+prep(ante).
+prep(bajo).
+prep(cabe).
+prep(con).
+prep(contra).
+prep(de).
+prep(desde).
+prep(en).
+prep(entre).
+prep(hasta).
+prep(hacia).
+prep(para).
+prep(por).
+prep(según).
+prep(sin).
+prep(so).
+prep(sobre).
+prep(tras).
+
+/*
+EJEMPLOS
+oracion([el,hombre,come,una,manzana],[]).
+oracion([la,mujer,come,manzanas],[]).
+oracion([maría,come,una,manzana,roja],[]).
+oracion([juan,ama,a,maría],[]).
+*/
